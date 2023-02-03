1194974910be00792f485ade485c00004f7d1882
Jawad <jawad.elhidraoui.pro@gmail.com>
2023-02-03 17:05:07 +0100

    u

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,10 @@ import { HomeComponent } from './home/home.component';
 import { OffersComponent } from './offers/offers.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
+  {path: "", component: HomeComponent},
   {path: "about-us", component: AboutusComponent},
   {path: "contact", component: ContactComponent },
   {path: "our-offer", component: OffersComponent},
-  {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "**", redirectTo: "home", pathMatch: "full"}
 ];
 

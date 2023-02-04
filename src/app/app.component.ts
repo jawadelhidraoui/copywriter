0b8ad7af48c40397c4830201073b6188d011aacf
Jawad <jawad.elhidraoui.pro@gmail.com>
2023-02-04 15:52:08 +0100

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: `app.components.html`,
   styleUrls: [`app.components.css`]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'ng-portfolio-app';
+
+  ngAfterViewInit() {
+    document.addEventListener('wheel', function(event) {
+      if (event.ctrlKey) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    }, {passive: false});
+  }
 }
